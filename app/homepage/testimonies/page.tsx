9596e5e35099c72bd8045663e6e81b4dd1672db1
ariframadinata05 <arief.rama05@gmail.com>
2025-04-08 00:12:46 +0700

    Update page.tsx

--- app/homepage/testimonies/page.tsx
+++ app/homepage/testimonies/page.tsx
@@ -1,3 +1,40 @@
+// import { alice } from "@/app/ui/fonts";
+// import { fetchTestimoniesData } from "app/lib/testimonie";
+// import Image from "next/image";
+// import Header from "@/app/ui/testimonies/header";
+
+// export default async function TestimoniesPage() {
+//   const items = await fetchTestimoniesData();
+
+//   return (
+//     <div>
+//         <Header />
+
+//         <main className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-3 flex-wrap justify-center gap-6 pt-4 pb-10 px-32 bg-[#9B1928]">
+//             {items.map((item) => (
+//                 <div
+//                 key={item.name}
+//                 className="bg-[#F4AE38] text-black rounded-xl p-4 py-10 text-center shadow-lg border-gray-200 border-2"
+//                 >
+//                 <Image
+//                     src={item.image}
+//                     alt={item.name}
+//                     width={75}
+//                     height={50}
+//                     className="mb-2 mx-auto object-contain h-22 "
+//                 />
+//                 <h2 className={`text-2xl ${alice.className}`}>{item.name}</h2>
+
+//                 <p className={`text-xl text-black mt-4 ${alice.className}`}>
+//                     {item.text}
+//                 </p>
+//                 </div>
+//             ))}
+//         </main>
+//     </div>
+//   );
+
+
 import { alice } from "@/app/ui/fonts";
 import { fetchTestimoniesData } from "app/lib/testimonie";
 import Image from "next/image";
@@ -8,29 +45,36 @@ export default async function TestimoniesPage() {
 
   return (
     <div>
-        <Header />
+      <Header />
 
-        <main className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-3 flex-wrap justify-center gap-6 pt-4 pb-10 px-32 bg-[#9B1928]">
-            {items.map((item) => (
-                <div
-                key={item.name}
-                className="bg-[#F4AE38] text-black rounded-xl p-4 py-10 text-center shadow-lg border-gray-200 border-2"
-                >
-                <Image
-                    src={item.image}
-                    alt={item.name}
-                    width={75}
-                    height={50}
-                    className="mb-2 mx-auto object-contain h-22 "
-                />
-                <h2 className={`text-2xl ${alice.className}`}>{item.name}</h2>
+      <main className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 flex-wrap justify-center gap-6 pt-4 pb-10 px-32 bg-[#9B1928]">
+        {items.map((item) => (
+          <div
+            key={item.id}
+            className="bg-[#F4AE38] text-black rounded-xl p-4 py-10 text-center shadow-lg border-gray-200 border-2"
+          >
+            <Image
+              src={item.image}
+              alt={item.name}
+              width={75}
+              height={50}
+              className="mb-2 mx-auto object-contain h-22"
+            />
+            <h2 className={`text-2xl ${alice.className}`}>{item.name}</h2>
+            <p className={`text-xl text-black mt-4 ${alice.className}`}>
+              {item.text}
+            </p>
+          </div>
+        ))}
+      </main>
 
-                <p className={`text-xl text-black mt-4 ${alice.className}`}>
-                    {item.text}
-                </p>
-                </div>
-            ))}
-        </main>
+      {/* */}
+      <div className="flex justify-center mt-10">
+        <div className="border border-white bg-[#9B1928] p-4 rounded-lg">
+          <p className="text-white text-lg">+ Share</p>
+        </div>
+      </div>
     </div>
   );
 }
+// }
