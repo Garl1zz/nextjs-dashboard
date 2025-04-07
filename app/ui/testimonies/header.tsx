c7b31aa4c579f0191e13a69d4be3da4d6cc5a041
ariframadinata05 <arief.rama05@gmail.com>
2025-04-08 00:09:00 +0700

    Update header.tsx

--- app/ui/testimonies/header.tsx
+++ app/ui/testimonies/header.tsx
@@ -14,15 +14,29 @@
 
 // }
 
-import styles from "@/app/ui/styles/testimoniespict.module.css"
+// import styles from "@/app/ui/styles/testimoniespict.module.css"
+
+// export default function Header() {
+//   return (
+//     <main className= {styles.showcase}>
+//     <div className="flex min-w-full flex-col p-6 max-w-full">
+//         <h1>Testimonies</h1>
+//     </div>
+//       <div></div>
+//     </main>
+//   );
+// }
+
+
+// ar
+import styles from "@/app/ui/styles/testimoniespict.module.css";
 
 export default function Header() {
   return (
-    <main className= {styles.showcase}>
-    <div className="flex min-w-full flex-col p-6 max-w-full">
-        <h1>Testimonies</h1>
-    </div>
-      <div></div>
+    <main className={styles.showcase}>
+      <div className="flex min-w-full flex-col p-6 max-w-full">
+        <h1 className="text-4xl font-bold text-center text-white">Testimonies</h1>
+      </div>
     </main>
   );
 }
