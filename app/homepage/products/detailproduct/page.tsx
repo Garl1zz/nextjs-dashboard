0e7d01af8cf1abb68350ca626665b41e067a4ef3
MrG4but <ivanharyanto56858@gmail.com>
2025-04-19 18:14:12 +0700

    link pada gambar, arif nanti tambah bagian detail

---
+++ app/homepage/products/detailproduct/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { useSearchParams } from "next/navigation";
+import { useMemo } from "react";
+import Image from "next/image";
+import { alice } from "@/app/ui/fonts";
+
+export default function DetailProductPage() {
+    return (
+      <div>
+        <h1>Detail Product Page</h1>
+      </div>
+    );
+  }
+  
