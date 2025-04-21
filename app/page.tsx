e19ad098ca3c7aeff8743e33864bf5cd2d394afd
CelloUX <marselinocelo@gmail.com>
2025-04-21 13:53:48 +0700

    responsive login and register UI

--- app/page.tsx
+++ app/page.tsx
@@ -14,14 +14,14 @@ export default function Page() {
   return (
     <main className="flex min-h-screen bg-[#67151f] text-white font-serif items-center justify-center">
       <div className={` ${styles.shape}`}>
-        {/* Inside frame */}
         <div className={` ${styles.logpicture}`}></div>
-        <div className="w-full md:w-1/2 p-8 flex flex-col items-center justify-center">
+          <div className="w-full md:w-1/2 p-8 flex flex-col items-center justify-center">
           <div className="w-full md:w-1/2 p-8 flex flex-col justify-center">
             <h2 className={` ${bungee_inline.className} text-3xl font-bold text-white text-center mb-2`}>
               LOGIN
             </h2>
           </div>
+<<<<<<< Updated upstream
           <form className='justify-center space-y-4'>
           <div className="relative space-y-4">
             <input
@@ -49,6 +49,36 @@ export default function Page() {
                 </div>
                 
             <a href="/forgot-password" className={`${alice.className} underline text-white`}>
+=======
+          
+            <form className='justify-center space-y-4'>
+            <div className="relative space-y-4">
+              <input
+                type="text"
+                placeholder="Username"
+                className="w-full px-4 py-2  bg-[#d5a153] text-black placeholder-black font-serif rounded outline-none border border-black"
+              />
+
+              <input
+                type={showPassword ? "text" : "password"}
+                placeholder="Password"
+                className="w-full px-4 py-2 bg-[#d5a153] font-serif text-black placeholder-black rounded outline-none border border-black"
+              />
+              <button
+                type="button"
+                onClick={() => setShowPassword(!showPassword)}
+                className="absolute right-3 bottom-[10px] text-black"
+              >
+                {showPassword ? (
+                  <FaEyeSlash className="w-5 h-5" />
+                ) : (
+                  <FaEye className="w-5 h-5" />
+                )}
+              </button>
+            </div>
+
+            <a href="/lupapassword" className={`${alice.className} underline text-white`}>
+>>>>>>> Stashed changes
               Forgot Password?
             </a>
             <p className={`text-white text-sm mb-6`}>
@@ -65,11 +95,7 @@ export default function Page() {
               </Link>
             </div>
           </form>
-
-
-
-
-        </div>
+          </div>
       </div>
 
 
