8da988e999a0232fc48f0f3861165125371f112e
CelloUX <marselinocelo@gmail.com>
2025-05-20 14:45:35 +0700

    updated skeleton dashboard admin

--- app/ui/skeletons.tsx
+++ app/ui/skeletons.tsx
@@ -28,14 +28,16 @@ export function CardsSkeleton() {
     </>
   );
 }
-
+ 
 export function RevenueChartSkeleton() {
   return (
-    <div className={`${shimmer} relative w-full overflow-hidden md:col-span-4`}>
-      <div className="mb-4 h-8 w-36 rounded-md bg-gray-100" />
-      <div className="rounded-xl bg-gray-100 p-4">
-        <div className="sm:grid-cols-13 mt-0 grid h-[410px] grid-cols-12 items-end gap-2 rounded-md bg-white p-4 md:gap-4" />
+    <div className={`${shimmer} relative w-full m-10 pt-4 overflow-hidden md:col-span-4`}>
+      <div className="rounded-xl bg-gray-100 p-2"> 
+        <div className="flex w-full h-[400px] rounded-md bg-white ">
+          <div className="mt-4 ml-2 h-5 w-36 rounded-md bg-gray-200" />
+        </div>
       </div>
+      
     </div>
   );
 }
@@ -135,9 +137,8 @@ export default function DashboardSkeleton() {
         <CardSkeleton />
         <CardSkeleton />
       </div>
-      <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+      <div className="mt-6 flex gap-6 md:grid-cols-4 lg:grid-cols-8">
         <RevenueChartSkeleton />
-        <LatestInvoicesSkeleton />
       </div>
     </>
   );
